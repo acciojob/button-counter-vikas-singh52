31c0dc7218c80e67d34fd7a8afd1fb65047dde27
vikas-singh52 <vikaspanwar1206@gmail.com>
2024-02-08 19:56:40 +0530

    first vesion

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
+  const [count, setCount] = useState(0)
+  function changeCount(){
+    setCount(count+1)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Button clicked {count} times</p>
+        <button onClick={changeCount}>Click me</button>
     </div>
   )
 }
